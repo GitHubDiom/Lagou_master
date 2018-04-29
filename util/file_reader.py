44280bc7e8e732d5b696d30abf9f7817882fc0f4
GitHubDiom <867320164@qq.com>
2018-04-29 13:58:28 +0800

    更新遍历xml方式

--- util/file_reader.py
+++ util/file_reader.py
@@ -1,11 +1,19 @@
 from lxml import etree
-
-
+from collections import defaultdict
+import re
 def parse_job_xml(job_xml_filepath):
     """
     parse the job.xml file in res directory, and return job list
     :param job_xml_filepath:
     :return:
     """
+    catalog_job = defaultdict(list)
     tree = etree.parse(job_xml_filepath)
-    return tree.xpath('//job/text()')
+    job_catalog_tag = tree.xpath('//job_catalog')
+    for item in job_catalog_tag:
+        v = item.xpath('string(.)').split()
+        job_list = []
+        for i in range(1,len(v)):
+            job_list.append(v[i])
+        catalog_job[v[0]]=job_list
+    return catalog_job
