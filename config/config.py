b65dcef102441706c6556a42889247e173be1b31
GitHubDiom <867320164@qq.com>
2018-05-02 22:12:48 +0800

    添加所有文件需要的import

--- config/config.py
+++ config/config.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
-
 import os
+import re,os,time,requests,sys,traceback,json,csv
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
+from util.file_reader import parse_job_xml
+import pandas as pd
+from collections import Counter,defaultdict
+
+from pyecharts import Bar, Scatter3D,Pie,Line,Parallel,WordCloud,Geo,Page,Grid,Polar
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+from operator import itemgetter
+try:
+    from urllib import parse as parse
+except:
+    import urllib as parse
+import xlwt
+from util import log
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 TIME_SLEEP = 2
 
