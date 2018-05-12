cbc5bb9c02be7b5441a2a432fcf6383dd402a8a1
GitHubDiom <867320164@qq.com>
2018-05-13 00:01:27 +0800

    日常乱动

--- analysis/to_echarts.py
+++ analysis/to_echarts.py
@@ -8,13 +8,10 @@ from config.config import *
 def main_solve(): 
     csvFile =  open('../spider/data/Info.csv','r',encoding='utf-8')
     df = pd.read_csv(csvFile)
-    city_set = df.所在城市.unique()
-    job_set = df.职业类别.unique()
-    degree_set  = df.最低学历.unique()
+    city_set, job_set, degree_set = df.所在城市.unique(), df.职业类别.unique(), df.最低学历.unique()
     city_salary_avg = {city :int(df[df['所在城市']==city]['薪资待遇'].mean()) for city in city_set if int(df[df['所在城市']==city]['所在城市'].value_counts())>30}
     job_salary_avg = {job : int(df[df['职业类别']==job]['薪资待遇'].mean()) for job in job_set}
-    degree_salary_avg = {degree : int(df[df.最低学历== degree].薪资待遇.mean()) for degree in degree_set}
-    
+    degree_salary_avg = {degree : int(df[df.最低学历== degree].薪资待遇.mean()) for degree in degree_set}   
     page = Page()
     #各城市平均月薪
     geo = Geo(title_color="white",height=550, background_color='#404a59')
@@ -23,6 +20,7 @@ def main_solve():
     page.add(geo)
 
     #各岗位热度
+
     pie_salary = Pie()
     key ,value = pie_salary.cast(dict(df.职业类别.value_counts()))
     pie_salary.add("", key, value, center=[50, 70], is_random=True, radius=[20, 55], is_legend_show=True, is_label_show=True)
